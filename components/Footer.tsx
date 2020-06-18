b1c7557aec2061b52ed5030d0968cc742e23a2c1
clayton-duarte <clayton@null.net>
2020-06-18 00:58:49 -0400

    :feature: Creates Footer

---
+++ components/Footer.tsx
@@ -0,0 +1,13 @@
+import { styled } from "../providers/theme";
+
+export default styled.footer`
+  background: ${(props) => props.theme.palette.PRIMARY};
+  color: ${(props) => props.theme.palette.BG};
+  justify-content: space-between;
+  place-items: center;
+  position: sticky;
+  font-size: 2rem;
+  display: flex;
+  padding: 1rem;
+  bottom: 0;
+`;
