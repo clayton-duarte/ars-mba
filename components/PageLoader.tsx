e66014809cffbd6f0350a5f727953ffc8959aebf
clayton-duarte <clayton@null.net>
2020-06-18 02:11:57 -0400

    :zap: Updates PageLoadr

--- components/PageLoader.tsx
+++ components/PageLoader.tsx
@@ -1,7 +1,58 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useEffect, useState } from "react";
+import {
+  GiPerspectiveDiceSixFacesOne,
+  GiPerspectiveDiceSixFacesTwo,
+  GiPerspectiveDiceSixFacesThree,
+  GiPerspectiveDiceSixFacesFour,
+  GiPerspectiveDiceSixFacesFive,
+  GiPerspectiveDiceSixFacesSix,
+  GiPerspectiveDiceSixFacesRandom,
+} from "react-icons/gi";
+
+import { styled } from "../providers/theme";
+
+const Wrapper = styled.aside`
+  color: ${(props) => props.theme.palette.PRIMARY};
+  justify-content: center;
+  align-items: center;
+  font-size: 8rem;
+  display: flex;
+  height: 100vh;
+`;
 
 const PageLoader: FunctionComponent = () => {
-  return <aside>Please wait...</aside>;
+  const [intervalId, setIntervalId] = useState<number>();
+  const [diceIndex, setDiceIndex] = useState<number>(0);
+
+  const diceList = [
+    <GiPerspectiveDiceSixFacesOne />,
+    <GiPerspectiveDiceSixFacesTwo />,
+    <GiPerspectiveDiceSixFacesThree />,
+    <GiPerspectiveDiceSixFacesFour />,
+    <GiPerspectiveDiceSixFacesFive />,
+    <GiPerspectiveDiceSixFacesSix />,
+    <GiPerspectiveDiceSixFacesRandom />,
+  ];
+
+  const startRoll = () => {
+    setIntervalId(
+      setInterval(() => {
+        const newDiceIndex = diceIndex + 1;
+        setDiceIndex(newDiceIndex < diceList.length ? newDiceIndex : 0);
+      }, 300)
+    );
+  };
+
+  const stopRoll = () => {
+    clearInterval(intervalId);
+  };
+
+  useEffect(() => {
+    startRoll();
+    return stopRoll;
+  }, [diceIndex]);
+
+  return <Wrapper>{diceList[diceIndex]}</Wrapper>;
 };
 
 export default PageLoader;
